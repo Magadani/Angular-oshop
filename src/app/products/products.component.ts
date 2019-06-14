a7bae7cc292758bf3ef04308321608f391280079
Johannes <johannes.magadani@gmail.com>
2019-06-14 14:18:37 -0700

    Adding Angular project to git

---
+++ src/app/products/products.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+import { ProductService } from '../product.service';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from '../model/product';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.css']
+})
+export class ProductsComponent {
+  products: Product[] = [];
+  filteredProducts: Product[] = [];
+  category: string;
+
+  constructor(
+    route: ActivatedRoute,
+    productService: ProductService 
+    ) { 
+
+    productService.getAll().subscribe(products => {
+      this.products = products;
+
+      route.queryParamMap.subscribe(params =>{
+        this.category = params.get('category');
+  
+        this.filteredProducts = (this.category) ? 
+        this.products.filter(p => p.category === this.category):
+        this.products;
+  
+      });
+    });
+    
+
+    
+  }
+
+}
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyA7uV39-5id7C81oSrYBP8dz1XNZzBcQvs",
+    authDomain: "oshop-edf35.firebaseapp.com",
+    databaseURL: "https://oshop-edf35.firebaseio.com",
+    projectId: "oshop-edf35",
+    storageBucket: "oshop-edf35.appspot.com",
+    messagingSenderId: "1057756380341"
+  }
+};
